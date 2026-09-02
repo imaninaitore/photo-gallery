e50f0d826b48d9e988af5ebd1cdb696e50297588
imaninaitore <imaninaitore1726@gmail.com>
2026-09-02 22:07:01 +0300

    create photo model

--- photo_gallery/models.py
+++ photo_gallery/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='photos/')
+    tags = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
