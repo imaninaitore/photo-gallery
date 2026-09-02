e2cdeb479401fe9a77ece50dcab75a6dd6695ced
imaninaitore <imaninaitore1726@gmail.com>
2026-09-02 23:03:39 +0300

    add registration view and import fro forms

--- photo_gallery/views.py
+++ photo_gallery/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Photo
+# Import the registration form we just created.
+from .forms import RegisterForm
 
 # Create your views here.
 def home(request):
@@ -16,3 +18,36 @@ def photo_detail(request, photo_id):
     photo = Photo.objects.get(id=photo_id)
 
     return render(request, "photo_detail.html", {"photo": photo})
+
+# REGISTER PAGE
+# -------------------------
+
+def register(request):
+
+    # Check whether the user submitted the form.
+    if request.method == "POST":
+
+        # Put the submitted information into our form.
+        form = RegisterForm(request.POST)
+
+        # Check whether the submitted information is valid.
+        if form.is_valid():
+
+            # Save the new user to the database.
+            form.save()
+
+            # After registration, send the user to the login page.
+            return redirect("login")
+
+    else:
+
+        # If the user is simply visiting the page,
+        # create an empty registration form.
+        form = RegisterForm()
+
+    # Display register.html and send the form to it.
+    return render(
+        request,
+        "register.html",
+        {"form": form}
+    )
